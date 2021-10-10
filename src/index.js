a0222c0aca8bafab5ee2df6ae8c8c4457e8d3f00
manujagobind <manujagobind@gmail.com>
2021-10-10 22:22:43 +0530

    Handle winner

--- src/index.js
+++ src/index.js
@@ -19,8 +19,33 @@ class Board extends React.Component {
         };
     }
 
-    handleClick(i) {
+    calculateWinner(squares) {
+        // Given an array of 9 squares, return 'X', 'O', or null as appropriate
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length ; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    handleClick(i) {        
         const squares = this.state.squares.slice();
+        if (this.calculateWinner(squares) || squares[i]) {
+            // Return if there's already a winner or square has a value
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -38,7 +63,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+            status = 'Winner ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
         <div>
