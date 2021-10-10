27dbb09a5ff12b2787100d4e592711ad433d459d
manujagobind <manujagobind@gmail.com>
2021-10-10 21:40:32 +0530

    child component calls parent's function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={() => this.props.onClick()}>
           {this.props.value}
         </button>
       );
@@ -20,8 +20,19 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
